3dc1e4542c4990d6ba745eca422744dfcb06edfa
hasansufiyan <hasansufiyan.xworkz@gmail.com>
2023-01-11 10:08:00 -0800

    git ignore constants

---
+++ Constants.gitignore/constants/SqlCredentials.java
@@ -0,0 +1,28 @@
+package com.xworkz.constants;
+
+public enum SqlCredentials {
+	
+
+		
+		URL("jdbc:mysql://localhost:3306/ Xworkz"),USERNAME("root"),PASSWORD("Xworkzodc@123");
+
+		public String value;
+		
+		SqlCredentials(String value) {
+			this.value=value;
+		}
+
+		public String getValue(String value) {
+			return value;
+		}
+		
+		
+	}
+
+#ignore ALL .log files
+*.log
+
+
+#ignore ALL files in ANY directory named temp
+temp/
+
